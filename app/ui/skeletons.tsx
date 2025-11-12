8656c9717ef628cf544b3c17608b330ffe87eceb
kurt_ <pandaykurt427@gmail.com>
2025-11-12 21:17:26 +0800

    added the loading skeleton for Card, CardsWrapper, TableRow, TeachersMobile, TeacherTable

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -29,7 +29,6 @@ export function CardsWrapperSkeleton() {
   );
 }
 
-
 export function TableRowSkeleton() {
   return (
     <tr className="w-full border-b border-gray-100 last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
@@ -67,7 +66,7 @@ export function TableRowSkeleton() {
   );
 }
 
-export function InvoicesMobileSkeleton() {
+export function TeachersMobileSkeleton() {
   return (
     <div className="mb-2 w-full rounded-md bg-white p-4">
       <div className="flex items-center justify-between border-b border-gray-100 pb-8">
@@ -91,36 +90,36 @@ export function InvoicesMobileSkeleton() {
   );
 }
 
-export function InvoicesTableSkeleton() {
+export function TeacherTableSkeleton() {
   return (
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
-            <InvoicesMobileSkeleton />
-            <InvoicesMobileSkeleton />
-            <InvoicesMobileSkeleton />
-            <InvoicesMobileSkeleton />
-            <InvoicesMobileSkeleton />
-            <InvoicesMobileSkeleton />
+            <TeachersMobileSkeleton />
+            <TeachersMobileSkeleton />
+            <TeachersMobileSkeleton />
+            <TeachersMobileSkeleton />
+            <TeachersMobileSkeleton />
+            <TeachersMobileSkeleton />
           </div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
-                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  Customer
+                <th scope="col" className="px-4 py-5 font-light sm:pl-6">
+                  First Name
                 </th>
-                <th scope="col" className="px-3 py-5 font-medium">
+                <th scope="col" className="px-3 py-5 font-light">
+                  Middle Name
+                </th>
+                <th scope="col" className="px-3 py-5 font-light">
+                  Last Name
+                </th>
+                <th scope="col" className="px-3 py-5 font-light">
                   Email
                 </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Amount
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Date
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Status
+                <th scope="col" className="px-3 py-5 font-light">
+                  Specialization
                 </th>
                 <th
                   scope="col"
