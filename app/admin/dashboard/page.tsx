4dce89a48ccc1e39df793ec5327f68d8d13510df
kurt_ <pandaykurt427@gmail.com>
2025-11-12 20:52:04 +0800

    resized the dashboard text

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function page() {
   return (
     <main className="border">
-      <h1 className="text-6xl font-bold ml-4 mb-4 bg-linear-to-r from-hex-blue via-[#398efd] to-[#ffdca8] bg-clip-text text-transparent">
+      <h1 className="text-3xl font-bold ml-4 mb-4 bg-linear-to-r from-hex-blue via-[#398efd] to-[#ffdca8] bg-clip-text text-transparent">
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
