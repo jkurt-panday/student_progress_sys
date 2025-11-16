d7755bc00d8811beb637b284e8db7be487a96e47
kurt_ <pandaykurt427@gmail.com>
2025-11-16 18:16:54 +0800

    added the edit form component for teachers

---
+++ app/admin/teacher/[id]/edit/page.tsx
@@ -0,0 +1,29 @@
+import { fetchTeacherById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/teacher/breadcrumbs"
+import Form from "@/app/ui/teacher/edit-form";
+
+
+export default async function Page(
+    props: { params: Promise<{ id: string }>}
+) {
+    const params = await props.params;
+    const id = params.id;
+
+    const teacher = await fetchTeacherById(id);
+
+    return (
+        <main>
+            <Breadcrumbs 
+                breadcrumbs={[
+                    {label: 'Teacher', href: '/admin/teacher'},
+                    {label: 'Edit Teacher',
+                        href: `admin/dashboard/teacher${id}/edit`,
+                        active: true,
+                    }
+                ]}
+            />
+            {/* editing form */}
+            <Form teachers={teacher}/>
+        </main>
+    )
+}
