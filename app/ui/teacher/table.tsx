ae2479850e064cd79b6cda53c6f66806a19136db
kurt_ <pandaykurt427@gmail.com>
2025-11-18 21:18:41 +0800

    added the table for GradeLevel for the admin dashboard

--- app/ui/teacher/table.tsx
+++ app/ui/teacher/table.tsx
@@ -1,5 +1,7 @@
 // import Image from "next/image"
-import { fetchFilteredTeachers } from "@/app/lib/data";
+import { fetchFilteredTeachers,
+         fetchGradeLevels
+ } from "@/app/lib/data";
 import { UpdateTeacher, DeleteTeacher } from "../button";
 
 export default async function TeacherTable({
@@ -9,7 +11,7 @@ export default async function TeacherTable({
 }) {
     const teachers = await fetchFilteredTeachers(query, currentPage);
 
-    // TODO delete button and functionality
+    // TODO the grade level CRUD
 
     return (
         <>
@@ -56,8 +58,8 @@ export default async function TeacherTable({
                                     </div>
                                 ))
                             }
-
                         </div>
+
                         {/* desktop version */}
                         <table className="hidden min-w-full text-gray-900 md:table">
                             <thead className="rounded-lg text-left text-sm font-normal">
@@ -122,6 +124,129 @@ export default async function TeacherTable({
                             ))}
                             </tbody>
                         </table>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export async function GradeLevelTable() {
+    const gradelevels = await fetchGradeLevels()
+
+    return (
+        <>
+            <div className="mt-6 flow-root">
+                <div className="inline-block min-w-full align-middle">
+                    <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+                        {/* mobile version */}
+                        <div className="md:hidden">
+                            {
+                                gradelevels?.map((gradelvl) => (
+                                    <div
+                                        key={gradelvl.gradeid}
+                                        className="mb-2 w-full rounded-md bg-white p-4"
+                                    >
+                                        <div className="flex items-center justify-between border-b pb-4">
+                                        <div>
+                                            <div className="mb-2 flex items-center">
+                                            {/* <Image
+                                                // src={teacher.image_url}
+                                                className="mr-2 rounded-full"
+                                                width={28}
+                                                height={28}
+                                                alt={`${teacher.name}'s profile picture`}
+                                            /> */}
+                                            <p>{gradelvl.gradename} </p>
+                                            </div>
+                                            <p className="text-sm text-gray-500">Assigned Teacher: </p>
+                                            <p className="text-sm text-gray-500">{gradelvl.firstname} {gradelvl.middlename} {gradelvl.lastname}</p>
+                                        </div>
+                                        {/* <InvoiceStatus status={teacher.status} /> */}
+                                        </div>
+                                        <div className="flex w-full items-center justify-between pt-4">
+                                        <div>
+                                            <p className="text-xl font-medium">
+                                            {/* {formatCurrency(teacher.amount)} */}
+                                            </p>
+                                            {/* <p>{formatDateToLocal(teacher.date)}</p> */}
+                                        </div>
+                                        <div className="flex justify-end gap-2">
+                                            <UpdateTeacher id={gradelvl.gradeid} />
+                                            <DeleteTeacher id={gradelvl.gradeid} />
+                                        </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+
+                        {/* desktop version */}
+                        <table className="hidden min-w-full text-gray-900 md:table">
+                            <thead className="rounded-lg text-left text-sm font-normal">
+                            <tr>
+                                <th scope="col" className="px-4 py-5 font-bold sm:pl-6">
+                                Grade Name
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-bold">
+                                Assigned Teacher
+                                </th>
+                                {/* <th scope="col" className="px-3 py-5 font-bold">
+                                Last Name
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-bold">
+                                Email
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-bold">
+                                Specialization
+                                </th> */}
+                                <th scope="col" className="relative py-3 pl-6 pr-3">
+                                <span className="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody className="bg-white">
+                            {gradelevels?.map((gradelvl) => (
+                                <tr
+                                key={gradelvl.gradeid}
+                                className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                                >
+                                <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                                    <div className="flex items-center gap-3">
+                                    {/* <Image
+                                        src={teacher.image_url}
+                                        className="rounded-full"
+                                        width={28}
+                                        height={28}
+                                        alt={`${teacher.name}'s profile picture`}
+                                    /> */}
+                                    <p>{gradelvl.gradename}</p>
+                                    </div>
+                                </td>
+                                <td className="whitespace-nowrap px-3 py-3">
+                                    {gradelvl.firstname} {gradelvl.middlename} {gradelvl.lastname}
+                                </td>
+                                {/* <td className="whitespace-nowrap px-3 py-3">
+                                    {gradelvl.middlename}
+                                </td>
+                                <td className="whitespace-nowrap px-3 py-3">
+                                    {gradelvl.lastname}
+                                </td> */}
+                                {/* <td className="whitespace-nowrap px-3 py-3">
+                                    {gradelvl.specialization}
+                                </td> */}
+                                <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                                    <div className="flex justify-end gap-3">
+                                    <UpdateTeacher id={gradelvl.gradeid} />
+                                    <DeleteTeacher id={gradelvl.gradeid} />
+                                    </div>
+                                </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+
                     </div>
                 </div>
             </div>
