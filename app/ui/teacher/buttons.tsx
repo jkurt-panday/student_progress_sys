f6d8c71157327f641c55d0e50b591992680c2c31
kurt_ <pandaykurt427@gmail.com>
2025-11-18 21:17:06 +0800

    added the CreateGradeLevel() button

--- app/ui/teacher/buttons.tsx
+++ app/ui/teacher/buttons.tsx
@@ -1,4 +1,8 @@
-import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
+import { 
+  // PencilIcon, 
+  PlusIcon, 
+  // TrashIcon 
+} from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 // import { deleteInvoice } from '@/app/lib/actions';    // chapter 12, mutating data
@@ -10,6 +14,18 @@ export function CreateTeacher() {
       className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
     >
       <span className="hidden md:block">Create Teacher</span>{' '}
+      <PlusIcon className="h-5 md:ml-4" />
+    </Link>
+  );
+}
+
+export function CreateGradeLevel() {
+  return (
+    <Link
+      href="/admin/gradelevel/create"
+      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+    >
+      <span className="hidden md:block">Create Grade Level</span>{' '}
       <PlusIcon className="h-5 md:ml-4" />
     </Link>
   );
