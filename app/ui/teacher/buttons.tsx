f98b08199b08aa311eb6e1bed6fd1dd16fa4a971
kurt_ <pandaykurt427@gmail.com>
2025-11-25 17:53:19 +0800

    added the create student func

--- app/ui/teacher/buttons.tsx
+++ app/ui/teacher/buttons.tsx
@@ -30,3 +30,15 @@ export function CreateGradeLevel() {
     </Link>
   );
 }
+
+export function CreateStudent() {
+  return (
+    <Link
+      href="/admin/student/create"
+      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+    >
+      <span className="hidden md:block">Create Student</span>{' '}
+      <PlusIcon className="h-5 md:ml-4" />
+    </Link>
+  );
+}
