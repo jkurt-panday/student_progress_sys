6050764c33181af5391cb7c3ed25afda86ebfbe1
kurt_ <pandaykurt427@gmail.com>
2025-11-26 17:18:24 +0800

    added the student table

--- app/ui/teacher/table.tsx
+++ app/ui/teacher/table.tsx
@@ -1,8 +1,8 @@
 // import Image from "next/image"
-import { fetchFilteredTeachers,
+import { fetchFilteredStudents, fetchFilteredTeachers,
          fetchGradeLevels
  } from "@/app/lib/data";
-import { UpdateTeacher, DeleteTeacher, UpdateGradelevel, DeleteGradelevel} from "../button";
+import { UpdateTeacher, DeleteTeacher, UpdateGradelevel, DeleteGradelevel, UpdateStudent, DeleteStudent} from "../button";
 
 export default async function TeacherTable({
     query, currentPage,
@@ -10,8 +10,6 @@ export default async function TeacherTable({
     query: string, currentPage: number
 }) {
     const teachers = await fetchFilteredTeachers(query, currentPage);
-
-    
 
     return (
         <>
@@ -230,6 +228,134 @@ export async function GradeLevelTable() {
                             </tbody>
                         </table>
 
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export async function StudentTable({
+    query, currentPage,
+}: {
+    query: string, currentPage: number
+}) {
+    const students = await fetchFilteredStudents (query, currentPage);
+
+    return (
+        <>
+            <div className="mt-6 flow-root">
+                <div className="inline-block min-w-full align-middle">
+                    <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+                        {/* mobile version */}
+                        <div className="md:hidden">
+                            {
+                                students?.map((student) => (
+                                    <div
+                                        key={student.studentid}
+                                        className="mb-2 w-full rounded-md bg-white p-4"
+                                    >
+                                        <div className="flex items-center justify-between border-b pb-4">
+                                        <div>
+                                            <div className="mb-2 flex items-center">
+                                            {/* <Image
+                                                // src={teacher.image_url}
+                                                className="mr-2 rounded-full"
+                                                width={28}
+                                                height={28}
+                                                alt={`${teacher.name}'s profile picture`}
+                                            /> */}
+                                            <p>{student.firstname} {student.middlename} {student.lastname}</p>
+                                            </div>
+                                            <p className="text-sm text-gray-500">Student Number: {student.studentnum}</p>
+                                            <p className="text-sm text-gray-500">LRN: {student.lrn}</p>
+                                            <p className="text-sm text-gray-500">Guardian: {student.guardianname}</p>
+                                            <p className="text-sm text-gray-500">Contact: {student.guardiancontact}</p>
+                                        </div>
+                                        {/* <InvoiceStatus status={teacher.status} /> */}
+                                        </div>
+                                        <div className="flex w-full items-center justify-between pt-4">
+                                        <div>
+                                            <p className="text-xl font-medium">
+                                            {/* {formatCurrency(teacher.amount)} */}
+                                            </p>
+                                            {/* <p>{formatDateToLocal(teacher.date)}</p> */}
+                                        </div>
+                                        <div className="flex justify-end gap-2">
+                                            <UpdateStudent id={student.studentid} />
+                                            <DeleteStudent id={student.studentid} />
+                                        </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+
+                        {/* // TODO make the data fit in the table */}
+                        {/* desktop version */}
+                        <table className="hidden min-w-full text-gray-900 md:table">
+                            <thead className="rounded-lg text-left text-sm font-normal">
+                            <tr>
+                                <th scope="col" className="px-4 py-5 font-bold sm:pl-6">
+                                First Name
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-bold">
+                                Middle Name
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-bold">
+                                Last Name
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-bold">
+                                Age
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-bold">
+                                Sex
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-bold">
+                                Grade level
+                                </th>
+                                <th scope="col" className="relative py-3 pl-6 pr-3">
+                                <span className="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody className="bg-white">
+                            {students?.map((student) => (
+                                <tr
+                                key={student.studentid}
+                                className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                                >
+                                <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                                    <div className="flex items-center gap-3">
+                                    {/* <Image src={teacher.image_url}className="rounded-full" width={28} height={28} alt={`${teacher.name}'s profile picture`} /> */}
+                                    <p>{student.firstname}</p>
+                                    </div>
+                                </td>
+                                <td className="whitespace-nowrap px-3 py-3">
+                                    {student.middlename}
+                                </td>
+                                <td className="whitespace-nowrap px-3 py-3">
+                                    {student.lastname}
+                                </td>
+                                <td className="whitespace-nowrap px-3 py-3">
+                                    {student.age}
+                                </td>
+                                <td className="whitespace-nowrap px-3 py-3 capitalize">
+                                    {student.sex}
+                                </td>
+                                <td className="whitespace-nowrap px-3 py-3">
+                                    {student.gradename}
+                                </td>
+                                <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                                    <div className="flex justify-end gap-3">
+                                    <UpdateStudent id={student.studentid} />
+                                    <DeleteStudent id={student.studentid} />
+                                    </div>
+                                </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
